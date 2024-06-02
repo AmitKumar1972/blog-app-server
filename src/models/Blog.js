3f56edd1551ac7acf548bf87e5e710ffcd24e241
Amit Kumar <amitgodara1008@gmail.com>
2024-06-02 13:13:34 +0530

    create blog and comment model

---
+++ src/models/Blog.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose');
+
+const BlogSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  body: {
+    type: String,
+    required: true,
+  },
+  author: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+  comments: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Comment',
+    },
+  ],
+});
+
+module.exports = mongoose.model('Blog', BlogSchema);
