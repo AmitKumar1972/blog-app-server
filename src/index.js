0e28f7be3a0f497daf6181e13cc0bb012fa1b594
Amit Kumar <amitgodara1008@gmail.com>
2024-06-02 13:11:06 +0530

    create user model and login/register api

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ const cors = require('cors');
 const bodyParser = require('body-parser');
 const connectDB = require('./config/db');
 const logger = require('./config/logger');
+const authRoutes = require('./routes/auth');
 
 const app = express();
 
@@ -11,6 +12,21 @@ connectDB();
 
 app.use(cors());
 app.use(bodyParser.json());
+
+// Log incoming requests
+app.use((req, res, next) => {
+  logger.info(`Incoming request: ${req.method} ${req.url}`);
+  next();
+});
+
+// Define routes
+app.use('/api/auth', authRoutes);
+
+// Error handling middleware
+app.use((err, req, res, next) => {
+  logger.error(err.stack);
+  res.status(500).send('Something broke!');
+});
 
 
 const PORT = process.env.PORT || 5000;
